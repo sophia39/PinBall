1d88083fb7699d8b7c5f7489b13eece1f6d89695
sophia39 <sfidante.kanna@gmail.com>
2018-01-16 13:54:07 +0900

    pinballプロジェクト　レッスン５の課題
    score表示のC#をコーディングしました。

---
+++ Assets/Score.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    int point; //スコアポイント
+    int smallstirpoint;
+    int largestirpoint;
+    int smallcloudpoint;
+    int largecloudpoint;
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のScoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+        //スコアを表示
+        this.scoreText.GetComponent<Text>().text = "Score:" + point;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //衝突に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        // タグによってスコアを変える
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            smallstirpoint += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            largestirpoint += 20;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            smallcloudpoint += 30;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            largecloudpoint += 40;
+        }
+        //スコア計算
+        point = smallstirpoint + largestirpoint + smallcloudpoint + largecloudpoint;
+        //スコアを表示
+        this.scoreText.GetComponent<Text>().text = "Score:" + point;
+
+    }
+}
